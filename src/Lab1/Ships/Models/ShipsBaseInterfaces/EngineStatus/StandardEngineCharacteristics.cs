add6b384b4ec0c93a7d4c2cf3bcf82dbc8502f52
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-09-21 18:07:54 +0300

    refactor: changed name ConstantEngineSpeed to StandardEngineCharacteristics

--- src/Lab1/Ships/Models/ShipsBaseInterfaces/EngineStatus/StandardEngineCharacteristics.cs
+++ src/Lab1/Ships/Models/ShipsBaseInterfaces/EngineStatus/StandardEngineCharacteristics.cs
@@ -1,6 +1,6 @@
 ﻿namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsBaseInterfaces.EngineStatus;
 
-public enum ConstantEngineSpeed
+public enum StandardEngineCharacteristics
 {
     None = 0,
     StandardConstantSpeed = 100,
