bdf4e3a72711ae3f85106ed23c953ba7d7aba509
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-09-21 20:46:27 +0300

    feat: add HealthPointHull.cs and HealthPointsDeflector.cs to Models.StandardSpecifications

---
+++ src/Lab1/Ships/Models/StandardSpecifications/HealthPointHull.cs
@@ -0,0 +1,9 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.StandardSpecifications;
+
+public enum HealthPointHull
+{
+    HullBrokenHealth,
+    HullFirstHealth = 15,
+    HullSecondHealth = 100,
+    HullThirdHealth = 205,
+}
---
+++ src/Lab1/Ships/Models/StandardSpecifications/HealthPointsDeflector.cs
@@ -0,0 +1,10 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.StandardSpecifications;
+
+public enum HealthPointsDeflector
+{
+    DeflectorBrokenHealth,
+    DeflectorPhotonsHealth = 3,
+    DeflectorFirstHealth = 20,
+    DeflectorSecondHealth = 100,
+    DeflectorThirdHealth = 400,
+}
