827b1982c4089cc20c2e821f6d9357f87a3955ce
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-10-03 00:14:54 +0300

    feat: add WeightHull.cs

---
+++ src/Lab1/Ships/Models/StandardSpecifications/WeightHull.cs
@@ -0,0 +1,9 @@
+﻿namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.StandardSpecifications;
+
+public enum WeightHull
+{
+    None,
+    FirstHullWeight = 1000,
+    SecondHullWeight = 1500,
+    ThirdHullWeight = 2000,
+}
