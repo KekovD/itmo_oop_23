a1fbe425824460de2a744e3a2894623bdb64bb92
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-10-02 22:11:20 +0300

    feat: GammaJumpEngine.cs and OmegaJumpEngine.cs

---
+++ src/Lab1/Ships/Entities/EnginesEntities/JumpEntities/GammaJumpEngine.cs
@@ -0,0 +1,14 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.EnginesModels;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.StandardSpecifications;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.EnginesEntities.JumpEntities;
+
+public class GammaJumpEngine : BaseJumpEngines
+{
+    public GammaJumpEngine()
+    {
+        TypeOfEngine = BaseEngineType.JumpEngine;
+        JumpRage = (int)JumpEngineCharacteristics.GammaDistance;
+        JumpFuelConsumption = (int)JumpEngineCharacteristics.GammaFlowRate;
+    }
+}
---
+++ src/Lab1/Ships/Entities/EnginesEntities/JumpEntities/OmegaJumpEngine.cs
@@ -0,0 +1,14 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.EnginesModels;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.StandardSpecifications;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.EnginesEntities.JumpEntities;
+
+public class OmegaJumpEngine : BaseJumpEngines
+{
+    public OmegaJumpEngine()
+    {
+        TypeOfEngine = BaseEngineType.JumpEngine;
+        JumpRage = (int)JumpEngineCharacteristics.OmegaDistance;
+        JumpFuelConsumption = (int)JumpEngineCharacteristics.OmegaFlowRate;
+    }
+}
