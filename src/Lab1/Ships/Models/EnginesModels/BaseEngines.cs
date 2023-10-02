dfd454466bb28aa6bbc9682a3f3c60330de1e609
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-10-03 00:17:44 +0300

    refactor: add protected set for public bool Running in BaseEngines.cs

--- src/Lab1/Ships/Models/EnginesModels/BaseEngines.cs
+++ src/Lab1/Ships/Models/EnginesModels/BaseEngines.cs
@@ -4,6 +4,6 @@ namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.EnginesModels;
 
 public abstract class BaseEngines : EngineTypeIdentification, ICanBeLaunched
 {
-    public bool Running { get; }
+    public bool Running { get; protected set; }
     public bool Serviceability { get; } = true;
 }
