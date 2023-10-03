de5a7267973071b3edf05c82315e6df3a10bc5f3
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-10-04 00:43:09 +0300

    feat: add AugurShip.cs

---
+++ src/Lab1/Ships/Entities/ShipsEntities/AugurShip.cs
@@ -0,0 +1,27 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.PartEntities.DeflectorEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.PartEntities.EnginesEntities.ImpulseEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.PartEntities.EnginesEntities.JumpEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.PartEntities.HullEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.PartEntities.TankEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsModels;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.StandardSpecifications.TankSpecifications;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.ShipsEntities;
+
+public class AugurShip : BaseShipWithJumpEngineAndDeflector
+{
+    public AugurShip(int currentMoney, int currentStandardFuelResidue, int currentJumpFuelResidue, bool havePhotons)
+        : base(currentMoney)
+    {
+        ShipHull = new HullThird();
+        ShipStandardTank = new StandardTank(
+            (int)CapacityTankStandard.CapacityStandardAugur,
+            currentStandardFuelResidue);
+        ImpulseEngine = new EImpulseEngine();
+        ShipJumpTank = new JumpTank(
+            (int)CapacityTankJump.CapacityJumpAugur,
+            currentJumpFuelResidue);
+        JumpEngine = new AlphaJumpEngine();
+        ShipDeflector = new DeflectorThird(havePhotons);
+    }
+}
