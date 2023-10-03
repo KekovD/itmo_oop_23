09cd29e021e446971343de7f3aee05eebe2fc163
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-10-04 00:02:54 +0300

    feat: add MeredianShip.cs

---
+++ src/Lab1/Ships/Entities/ShipsEntities/MeredianShip.cs
@@ -0,0 +1,22 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.PartEntities.DeflectorEntities.StandardDeflectors;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.PartEntities.EnginesEntities.ImpulseEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.PartEntities.HullEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.PartEntities.TankEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsModels;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.StandardSpecifications.TankSpecifications;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.ShipsEntities;
+
+public class MeredianShip : BaseShipWithDeflector ////TODO: добавить излучатели(смотри тз)
+{
+    public MeredianShip(int currentMoney, int currentStandardFuelResidue)
+        : base(currentMoney)
+    {
+        ShipHull = new HullSecond();
+        ShipStandardTank = new StandardTank(
+            (int)CapacityTankStandard.CapacityStandardWalkingShuttle,
+            currentStandardFuelResidue);
+        ImpulseEngine = new EImpulseEngine();
+        ShipDeflector = new DeflectorStandardSecond();
+    }
+}
---
+++ src/Lab1/Ships/Models/ShipsModels/BaseShipWithDeflector.cs
@@ -0,0 +1,14 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.DeflectorsModels;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsBaseInterfaces.ShipInterfaces;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsModels;
+
+public abstract class BaseShipWithDeflector : BaseShip, IShipDeflector
+{
+    protected BaseShipWithDeflector(int currentMoney)
+        : base(currentMoney)
+    {
+    }
+
+    public BaseDeflector ShipDeflector { get; protected init; } = new BaseDeflector();
+}
