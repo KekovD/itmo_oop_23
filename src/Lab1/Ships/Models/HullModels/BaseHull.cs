f84dbdaf6742aac300c261b649ca674db68e8666
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-10-04 20:47:35 +0300

    refactor: delete standard health initialization in BaseHull.cs

--- src/Lab1/Ships/Models/HullModels/BaseHull.cs
+++ src/Lab1/Ships/Models/HullModels/BaseHull.cs
@@ -1,12 +1,11 @@
 ﻿using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsBaseInterfaces;
 using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsBaseInterfaces.HullInterfaces;
-using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.StandardSpecifications.HullSpecifications;
 
 namespace Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.HullModels;
 
 public class BaseHull : IHullHealthPoint, IPartWeight
 {
-    public int HealthOfHull { get; protected set; } = (int)HealthPointHull.HullBrokenHealth;
+    public int HealthOfHull { get; protected set; }
     public bool Serviceability { get; protected set; } = true;
     public int PartWeight { get; protected init; }
 }
