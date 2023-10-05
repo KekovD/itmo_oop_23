c96c56d363d8221b71fb0de53d02326511509969
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-10-05 14:35:23 +0300

    feat: add SpaceWhales.cs

---
+++ src/Lab1/Environment/Models/ObstaclesModels/SpaceWhales.cs
@@ -0,0 +1,18 @@
+﻿using Itmo.ObjectOrientedProgramming.Lab1.Environment.Models.StandardSpecifications;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsModels;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Environment.Models.ObstaclesModels;
+
+public abstract class SpaceWhales : AntimatterFlashes
+{
+    public override void DoingDamage(BaseShip ship, int damage)
+    {
+        if (ship.ShipAntiNitrinoEmitter)
+        {
+            return;
+        }
+
+        ship.ShipHull.SetHealthOfHull(ship.ShipHull.HealthOfHull - (int)ObstructionDamage.SpaceWhaleDamage);
+        ship.SetShipAlive();
+    }
+}
