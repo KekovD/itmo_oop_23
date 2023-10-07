8cc898eb7255e679616c32548515474bc5990b62
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-10-07 03:29:10 +0300

    test: add six test

---
+++ tests/Lab1.Tests/Tests/SixthTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Entities.SpaceEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Models.EnvironmentsOfSpaceModels;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Services;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.ShipsEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsModels;
+using Xunit;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Tests.Tests;
+
+public class SixthTest
+{
+    private static bool CheckResult(IList<BaseShip> manyShips, IList<BaseSpace> manySpaces)
+    {
+        var service = new MainService();
+        var checkList = new List<List<string>>(service.MainLaunch(manyShips, manySpaces).Select(x => x.ToList()));
+        const string checkFirst = "VaklasShip";
+        bool result = checkFirst.Equals(checkList[2][0], StringComparison.Ordinal);
+        return result;
+    }
+
+    [Theory]
+    [ClassData(typeof(ParameterizedTests))]
+
+    private void ConditionCheck(BaseShip walkingShuttleShip, BaseShip vaklasShip)
+    {
+        var manyShips = new List<BaseShip>
+        {
+            walkingShuttleShip,
+            vaklasShip,
+        };
+        var obstacles = new Collection<int>();
+        obstacles.Add(0);
+        var space = new HighDensitySpaceNebulae(500, obstacles);
+        var manySpaces = new List<BaseSpace>();
+        manySpaces.Add(space);
+        Assert.True(CheckResult(manyShips, manySpaces));
+    }
+
+    private class ParameterizedTests : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[]
+            {
+                new WalkingShuttleShip(),
+                new VaklasShip(false),
+            },
+        };
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
