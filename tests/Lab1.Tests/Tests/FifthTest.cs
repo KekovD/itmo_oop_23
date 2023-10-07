c22a8a04dab89f71e71b956aeb0547f27efcaec6
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-10-07 03:16:17 +0300

    feat: add FifthTest.cs

---
+++ tests/Lab1.Tests/Tests/FifthTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Entities.SpaceEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Models.EnvironmentsOfSpaceModels;
+using Itmo.ObjectOrientedProgramming.Lab1.Environment.Services;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Entities.ShipsEntities;
+using Itmo.ObjectOrientedProgramming.Lab1.Ships.Models.ShipsModels;
+using Xunit;
+
+namespace Itmo.ObjectOrientedProgramming.Lab1.Tests.Tests;
+
+public class FifthTest
+{
+    private static bool CheckResult(IList<BaseShip> manyShips, IList<BaseSpace> manySpaces)
+    {
+        var service = new MainService();
+        var checkList = new List<List<string>>(service.MainLaunch(manyShips, manySpaces).Select(x => x.ToList()));
+        const string checkFirst = "StellaShip";
+        bool result = checkFirst.Equals(checkList[2][0], StringComparison.Ordinal);
+        return result;
+    }
+
+    [Theory]
+    [ClassData(typeof(ParameterizedTests))]
+
+    private void ConditionCheck(BaseShip augurShip, BaseShip stellaShip)
+    {
+        var manyShips = new List<BaseShip>
+        {
+            augurShip,
+            stellaShip,
+        };
+        var obstacles = new Collection<int>();
+        obstacles.Add(2);
+        var space = new HighDensitySpaceNebulae(2000, obstacles);
+        var manySpaces = new List<BaseSpace>();
+        manySpaces.Add(space);
+        Assert.True(CheckResult(manyShips, manySpaces));
+    }
+
+    private class ParameterizedTests : IEnumerable<object[]>
+    {
+        private readonly List<object[]> _data = new List<object[]>
+        {
+            new object[]
+            {
+                new AugurShip(true),
+                new StellaShip(true),
+            },
+        };
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
