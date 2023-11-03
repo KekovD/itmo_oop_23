e6fb3e3c1ff2127ba1e3b8c7093ec890f8bb8ad6
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-11-03 23:56:19 +0300

    feat: add adapter

--- src/Lab3/Messengers/Models/IMessenger.cs
+++ src/Lab3/Messengers/Models/IMessenger.cs
@@ -1,9 +1,8 @@
 ﻿using Itmo.ObjectOrientedProgramming.Lab3.RenderableMessagesIntegration.Models;
-using Itmo.ObjectOrientedProgramming.Lab3.Renderables.Models;
 
 namespace Itmo.ObjectOrientedProgramming.Lab3.Messengers.Models;
 
-public interface IMessenger : IRenderable, IRenderableMessageHandling
+public interface IMessenger : IRenderableMessageHandling
 {
     string Name { get; }
 }
