081b8796dd105e689528679367e29f3e070cda6b
Lavrenov Dmitry <lavrenov_d@niuitmo.ru>
2023-12-14 01:56:16 +0300

    feat: add AdminViewAllOperationsHistoryByCustomerService.cs and AdminViewAllOperationsHistoryByCustomerScenario.cs

---
+++ src/Lab5/Lab5.Application/Admins/AdminViewAllOperationsHistoryByCustomerService.cs
@@ -0,0 +1,23 @@
+﻿using Application.Abstractions.Repositories;
+using Application.Models.Operations;
+using Lab5.Application.Contracts.Admins;
+
+namespace Lab5.Application.Admins;
+
+public class AdminViewAllOperationsHistoryByCustomerService : IAdminViewAllOperationsHistoryByCustomerService
+{
+    private readonly IAdminViewOperationsHistoryRepository _repository;
+
+    public AdminViewAllOperationsHistoryByCustomerService(IAdminViewOperationsHistoryRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async IAsyncEnumerable<Operation> ViewAllOperationsHistoryByCustomer(long customerAccountId)
+    {
+        await foreach (Operation operation in _repository.GetAllOperationsHistoryByCustomerAccountId(customerAccountId))
+        {
+            yield return operation;
+        }
+    }
+}
---
+++ src/Lab5/Lab5.Presentation.Console/Scenarios/AdminScenarios/AdminViewAllOperationsHistoryByCustomerScenario.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Text;
+using Application.Models.Operations;
+using Lab5.Application.Contracts.Admins;
+using Spectre.Console;
+
+namespace Lab5.Presentation.Console.Scenarios.AdminScenarios;
+
+public class AdminViewAllOperationsHistoryByCustomerScenario : IScenario
+{
+    private readonly IAdminViewAllOperationsHistoryByCustomerService _service;
+
+    public AdminViewAllOperationsHistoryByCustomerScenario(IAdminViewAllOperationsHistoryByCustomerService service)
+    {
+        _service = service;
+    }
+
+    public string Name => "View all operation history by customer";
+
+    public async Task Run()
+    {
+        long accountId = AnsiConsole.Ask<long>("Enter customer account id");
+
+        var stringBuilder = new StringBuilder();
+
+        await foreach (Operation operation in _service.ViewAllOperationsHistoryByCustomer(accountId))
+        {
+            stringBuilder.AppendFormat(
+                CultureInfo.InvariantCulture,
+                "Operation ID: {0}, Account ID: {1}, Amount: {2}, Type: {3}, State: {4}, Date: {5}\n",
+                operation.OperationId,
+                operation.AccountId,
+                operation.Amount,
+                operation.Type,
+                operation.State,
+                operation.Date);
+
+            AnsiConsole.WriteLine(stringBuilder.ToString());
+            stringBuilder.Clear();
+        }
+
+        AnsiConsole.Ask<string>("Ok");
+    }
+}
